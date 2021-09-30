5f116d89d53f8f87b810942df3ccec1c981aaa8b
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-30 18:25:48 +0300

    create class Person and Car

--- Lesson_21/ViewController.swift
+++ Lesson_21/ViewController.swift
@@ -17,3 +17,31 @@ class ViewController: UIViewController {
 
 }
 
+class Person {
+    var name: String
+    var car: Car?
+    
+    init(name: String) {
+        self.name = name
+        print("Person allocated")
+    }
+    
+    deinit {
+        print("Person deallocated")
+    }
+}
+
+
+class Car {
+    var name: String
+    var owner: Person?
+    
+    init(name: String) {
+        self.name = name
+        print("Person allocated")
+    }
+    
+        deinit {
+            print("Person deallocated")
+        }
+}
