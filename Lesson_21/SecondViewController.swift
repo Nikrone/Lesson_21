2f1556fe7eb195781870c9c8ed90aabf0e1280b7
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-30 19:14:28 +0300

    create SecondViewController

---
+++ Lesson_21/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Lesson_21
+//
+//  Created by Evgeniy Nosko on 30.09.21.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- Lesson_21/ViewController.swift
+++ Lesson_21/ViewController.swift
@@ -7,53 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    var zhenya:Person?
-    var mazda:Car = Car(name: "Mazda")
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        zhenya = Person(name: "Zhenya", car: mazda)
-        zhenya?.car = mazda
-        mazda.owner = zhenya
-        
-        zhenya = nil
-        
-    }
-
-}
-
 
 // unowned используется тогда, когда точно уверены что не будет-nil!
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-class Person {
-    var name: String
-    unowned var car: Car
-    
-    init(name: String, car: Car) {
-        self.name = name
-        self.car = car
-        print("Person allocated")
     }
-    
-    deinit {
-        print("Person deallocated")
-    }
+
 }
 
 
-class Car {
-    var name: String
-    var owner: Person?
-    
-    init(name: String) {
-        self.name = name
-        print("Car allocated")
-    }
-    
-        deinit {
-            print("Car deallocated")
-        }
-}
+
